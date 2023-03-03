5ad4cfe508cec3ff7ea38b978a2e36202132055c
Lewis Foskett <2661355f@student.gla.ac.uk>
2023-03-03 15:08:43 +0000

    test push to lewis branch

--- WAD2_Group_2E/settings.py
+++ WAD2_Group_2E/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+#if u see this hello
+
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
