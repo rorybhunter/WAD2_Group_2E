4a942c683434ced574236cbd5b19050075476d7e
Lewis Foskett <2661355f@student.gla.ac.uk>
2023-03-04 21:20:12 +0000

    basic add_movie view, model for movies, form for movie

--- RaisinRatings/views.py
+++ RaisinRatings/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .forms import MovieForm
+
+
+def add_movie(request):
+    if request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('movies_list')  # replace movies_list with your own view name
+    else:
+        form = MovieForm()
+    return render(request, 'movies/add_movie.html', {'form': form})
