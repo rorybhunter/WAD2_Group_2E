b2369c203c67f7b971dca21ff51a477b48dcfe5e
Lewis Foskett <2661355f@student.gla.ac.uk>
2023-03-04 21:27:52 +0000

    didnt set max length for charfields, fixed. created add movie template.

--- RaisinRatings/models.py
+++ RaisinRatings/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # Create your models here.
 
 class Movie(models.Model):
-    movie_name = models.CharField(128)
+    movie_name = models.CharField(max_length=128, unique=True)
     movie_id = models.IntegerField()
     category_id = models.IntegerField()
-    main_actor = models.CharField(128)
+    main_actor = models.CharField(max_length=128)
     likes = models.IntegerField()
-    username = models.CharField(128)
-    poster = models.imageField()
+    username = models.CharField(max_length=128)
+    # poster = models.imageField()
