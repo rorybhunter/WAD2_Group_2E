ccd2eaef198fa0d44ce803eadfc6217fdf0e8001
“Rory <2655888H@student.gla.ac.uk>
2023-03-05 15:14:37 +0000

    Changed registration form to allow user_type selection

--- RaisinRatings/forms.py
+++ RaisinRatings/forms.py
@@ -13,4 +13,5 @@ class UserForm(forms.ModelForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
+
         fields = ('picture', 'user_type')
--- RaisinRatings/models.py
+++ RaisinRatings/models.py
@@ -10,7 +10,17 @@ class UserProfile(models.Model):
 
     # additional attributes we wish to store.
     picture = models.ImageField(upload_to='profile_images', blank=True)
-    user_type = models.CharField(max_length=50, unique=False)
+
+    USER_TYPE_CHOICES = (
+
+        ('COUCH_POTATO', "Couch Potato"),
+        ('CRITIC', "Critic"),
+        ('CREATOR', "Creator")
+    )
+
+    user_type = models.CharField(max_length=15,
+                          choices=USER_TYPE_CHOICES,
+                          default="COUCH_POTATO")
 
     def __str__(self):
         return self.user.username
