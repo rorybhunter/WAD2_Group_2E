47bc15c01f08f1f568ee2715f2866b145e6cd964
“Rory <2655888H@student.gla.ac.uk>
2023-03-05 15:01:55 +0000

    added index to RR.urls

--- RaisinRatings/urls.py
+++ RaisinRatings/urls.py
@@ -5,5 +5,6 @@ app_name = 'RaisinRatings'
 
 urlpatterns = [
     # path(<string to match>, <view to call>, <name, way to reference view, optional>)
+    path('',views.index, name='index'),
     path('register/', views.register, name='register'),
 ]
