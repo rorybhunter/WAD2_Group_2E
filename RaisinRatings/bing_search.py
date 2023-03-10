4bc2e663ad6a1b8a88938320089d29129f0cce69
“Rory <2655888H@student.gla.ac.uk>
2023-03-10 16:35:09 +0000

    ADDED SEARCH!

--- RaisinRatings/bing_search.py
+++ RaisinRatings/bing_search.py
@@ -5,7 +5,7 @@ import requests
 def read_bing_key():
     bing_api_key = None
     try:
-        with open('bing.key','r') as f:
+        with open('bing.key', 'r') as f:
             bing_api_key = f.readline().strip()
     except:
         try:
@@ -20,23 +20,55 @@ def read_bing_key():
 
 
 def run_query(search_terms):
-    bing_key = read_bing_key()
+    # subscription_key = read_bing_key()
+    # # search_url = "https://api.bing.microsoft.com/v7.0/search"
     # search_url = 'https://api.cognitive.microsoft.com/bing/v7.0/search'
+    # headers = {"Ocp-Apim-Subscription-Key": subscription_key}
+    # params = {"q": search_terms, "textDecorations": True, "textFormat": "HTML"}
+    # response = requests.get(search_url, headers=headers, params=params)
+    # response.raise_for_status()
+    # search_results = response.json()
+    #
+    # results = []
+    # for result in search_results['webPages']['value']:
+    #     results.append({
+    #         'title': result['name'],
+    #         'link': result['url'],
+    #         'summary': result['snippet'],
+    #     })
+    #
+    # return results
+    from pprint import pprint
+    import requests
+
+    # Add your Bing Search V7 subscription key and endpoint to your environment variables.
+    subscription_key = read_bing_key()
+
     search_url = "https://api.bing.microsoft.com/v7.0/search"
-    headers = {'Ocp-Apim-Subscription-Key': bing_key}
-    params = {'q': search_terms, 'textDecorations': True, 'textFormat':' HTML'}
 
-    # Issue the request, given the details above.
-    response = requests.get(search_url, headers=headers, params=params)
-    response.raise_for_status()
-    search_results = response.json()
+    # Construct a request
+    mkt = 'en-US'
+    params = { 'q': search_terms, 'mkt': mkt }
+    headers = { 'Ocp-Apim-Subscription-Key': subscription_key }
 
-    # With the response now in play, build up a Python list.
-    results = []
-    for result in search_results['webPages']['value']:
-        results.append({
-            'title': result['name'],
-            'link': result['url'],
-            'summary': result['snippet']
-        })
-    return results
+    # Call the API
+    try:
+        response = requests.get(search_url, headers=headers, params=params)
+        response.raise_for_status()
+
+        print("Headers:")
+        print(response.headers)
+
+        print("JSON Response: ")
+        pprint(response.json())
+        search_results = response.json()
+        results = []
+        for result in search_results['webPages']['value']:
+            results.append({
+                'title': result['name'],
+                'link': result['url'],
+                'summary': result['snippet'],
+            })
+        return results
+    except Exception as ex:
+        raise ex
