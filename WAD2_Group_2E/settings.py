32a40d91951916e08ec2f1471cd73e0f5553946b
Ninelle Cherp <2689344c@student.gla.ac.uk>
2023-03-22 18:24:48 +0000

    added allowed host

--- WAD2_Group_2E/settings.py
+++ WAD2_Group_2E/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-^n6g*fiwhhlgu-ddzzezb(o3m_*k%um($dd$+k^sa#f$)glcsg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://Nelle.pythonanywhere.com/']
 
 # Application definition
 
@@ -126,13 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [STATIC_DIR, ]
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [STATIC_DIR, ]
+
+# STATIC_URL = '/static/'
+# #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Login & Logout URLs
