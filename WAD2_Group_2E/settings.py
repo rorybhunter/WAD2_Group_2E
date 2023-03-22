668d8254e556918e7f4a146f4d16a1b797a63786
Lewis Foskett <2661355f@student.gla.ac.uk>
2023-03-22 18:56:53 +0000

    localhost is now allowed

--- WAD2_Group_2E/settings.py
+++ WAD2_Group_2E/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-^n6g*fiwhhlgu-ddzzezb(o3m_*k%um($dd$+k^sa#f$)glcsg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nelle.pythonanywhere.com']
+ALLOWED_HOSTS = ['nelle.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
